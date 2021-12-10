d616f2430911449a81ea9f1040931fa740061cd3
Juliana Burzlaff <juliana.burzlaff@hotmail.com>
2021-12-10 01:07:44 -0300

    style: add comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+// Packages
 import React from "react";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+
+//Components
 import { PaymentList, PaymentCreate, PaymentEdit } from "./components/Payment";
 import { SalesList, SalesCreate, SalesEdit } from "./components/Sales";
 import { ClientList, ClientCreate, ClientEdit } from "./components/_Clients";
+
+//Icons
 import UserIcon from "@material-ui/icons/Group";
 import ReceiptIcon from "@mui/icons-material/Receipt";
 import PaymentsIcon from "@mui/icons-material/Payments";
+
+// Configs
 import Header from "./Header";
 import authProvider from "./authProvider";
 
